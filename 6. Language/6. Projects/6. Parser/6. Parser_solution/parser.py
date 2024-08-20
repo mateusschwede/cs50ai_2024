66afd79a8029c35fd115b81edb05fd021638347a
mateusschwede <mateus1908.schwede@gmail.com>
2024-08-20 11:55:33 -0300

    parser.py atualizado

--- 6. Language/6. Projects/6. Parser/6. Parser_solution/parser.py
+++ 6. Language/6. Projects/6. Parser/6. Parser_solution/parser.py
@@ -1,6 +1,6 @@
+from re import split
 import nltk
 import sys
-import re
 
 TERMINALS = """
 Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
@@ -16,14 +16,35 @@ V -> "smiled" | "tell" | "were"
 """
 
 NONTERMINALS = """
-S -> NP VP | NP VP Conj NP VP | NP VP Conj VP
-NP -> N | Det N | Det AP N | P NP | NP P NP
-VP -> V | Adv VP | V Adv | VP NP | V NP Adv
-AP -> Adj | AP Adj
+S -> NP VP | NP JP NP | NP VP JP NP
+S -> JP NP VP | JP NP VP JP NP | NP VP JP NP
+S -> S Conj VP NP | S Conj S | JP S | S Conj VP JP NP
+NP -> N | NP Adv | NP JP NP | Adj NP
+VP -> V | Adv VP | VP Adv | VP NP
+JP -> P | Det | P Det
 """
 
 grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
 parser = nltk.ChartParser(grammar)
+
+
+def get_token_symbols(token: str):
+    return [p.lhs().symbol() for p in grammar.productions(None, token)]
+
+
+def print_sentence_debug(tokens: list[str]):
+    print("Chart:", parser.chart_parse(tokens).pretty_format())
+    print("\nText:", *tokens)
+    all_symbols = []
+    for token in tokens:
+        token_symbols = get_token_symbols(token)
+        all_symbols += token_symbols
+        print(
+            *token_symbols,
+            " - ",
+            token,
+        )
+    print("\nSymbols:", *all_symbols)
 
 
 def main():
@@ -32,7 +53,6 @@ def main():
             s = f.read()
     else:
         s = input("Sentence: ")
-
     s = preprocess(s)
 
     try:
@@ -40,32 +60,55 @@ def main():
     except ValueError as e:
         print(e)
         return
-    
+
     if not trees:
-        print("Could not parse sentence")
+        print("Could not parse sentence.")
         return
 
-    for t in trees:
-        t.pretty_print()
+    for tree in trees:
+        tree.pretty_print()
+
         print("Noun Phrase Chunks")
-        for np in np_chunk(t):
+        for np in np_chunk(tree):
             print(" ".join(np.flatten()))
 
 
-def preprocess(sentence):
-    vTest = re.compile('[a-zA-Z]')
-    tokens = nltk.word_tokenize(sentence)
-    return [entry.lower() for entry in tokens if vTest.match(entry)]
+def contains_some_alpha_chars(word: str):
+    return any(c.isalpha() for c in word.strip())
 
 
-def np_chunk(tree):
-    chunks = []
-    pTree = nltk.tree.ParentedTree.convert(tree)
+def preprocess(sentence: str):
+    return [
+        word
+        for word in nltk.word_tokenize(sentence.lower())
+        if contains_some_alpha_chars(word)
+    ]
 
-    for sTree in pTree.subtrees():
-        if sTree.label() == "N":
-            chunks.append(sTree.parent())
-    return chunks
+
+def is_np_tree(tree: nltk.Tree) -> bool:
+    return tree.label() == "NP"
+
+
+def is_np_chunk(tree: nltk.Tree) -> bool:
+    if not is_np_tree(tree):
+        return False
+
+    for subtree in tree.subtrees():
+        if tree == subtree:
+            continue
+
+        if is_np_tree(subtree):
+            return False
+    return True
+
+
+def np_chunk(tree: nltk.Tree):
+    np_chunks = [
+        np_subtree
+        for np_subtree in tree.subtrees(is_np_tree)
+        if is_np_chunk(np_subtree)
+    ]
+    return np_chunks
 
 
 if __name__ == "__main__":
